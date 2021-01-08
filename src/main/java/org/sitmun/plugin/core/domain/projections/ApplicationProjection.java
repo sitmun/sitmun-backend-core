41fefb5c853ff4e08ec7172f948940a2e67e2ed0
fjlopez <fjlopez@unizar.es>
2021-01-08 23:50:49 +0100

    #45 added situation map id to application projection; closes #45

---
+++ src/main/java/org/sitmun/plugin/core/domain/projections/ApplicationProjection.java
@@ -0,0 +1,92 @@
+package org.sitmun.plugin.core.domain.projections;
+
+import java.util.Date;
+import java.util.List;
+import org.sitmun.plugin.core.domain.Application;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.rest.core.config.Projection;
+
+/**
+ * Projections for REST views of an application.
+ */
+@Projection(name = "view", types = {Application.class})
+public interface ApplicationProjection {
+
+  @Value("#{target.id}")
+  Integer getId();
+
+  /**
+   * Application name.
+   */
+  @Value("#{target.name}")
+  String getName();
+
+  /**
+   * Application type (external or internal).
+   */
+  @Value("#{target.type}")
+  String getType();
+
+  /**
+   * Title to be shown in the browser and in the application when it is internal.
+   */
+  @Value("#{target.title}")
+  String getTitle();
+
+  /**
+   * CSS to use in this application when it is internal.
+   */
+  @Value("#{target.theme}")
+  String getTheme();
+
+  /**
+   * Scales to be used in this application when it is internal.
+   */
+  @Value("#{target.scales}")
+  List<String> getScales();
+
+  /**
+   * Projection to be used in this application when it is internal.
+   */
+  @Value("#{target.srs}")
+  String getSrs();
+
+  /**
+   * The JSP viewer to be loaded in this application when it is internal or a link to the
+   * external application.
+   */
+  @Value("#{target.jspTemplate}")
+  String getJspTemplate();
+
+  /**
+   * Wheb tge appliction is internal {@code True} if the application refreshes automatically;
+   * {@code False} if an "update map" button is required.
+   */
+  @Value("#{target.treeAutoRefresh}")
+  Boolean getTreeAutoRefresh();
+
+  /**
+   * Can access a "parent" territory.
+   */
+  @Value("#{target.accessParentTerritory}")
+  Boolean getAccessParentTerritory();
+
+  /**
+   * Can access a "children" territory.
+   */
+  @Value("#{target.accessChildrenTerritory}")
+  Boolean getAccessChildrenTerritory();
+
+  /**
+   * Situation map when the application is internal.
+   */
+  @Value("#{target.situationMap != null? target.situationMap.id : null}")
+  Integer getSituationMapId();
+
+  /**
+   * Created date.
+   */
+  @Value("#{target.createdDate}")
+  Date getCreatedDate();
+
+}
