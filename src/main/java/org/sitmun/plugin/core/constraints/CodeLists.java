8d408c7bc45ac29e419951f58b41f075d4726cbc
fjlopez <fjlopez@unizar.es>
2021-01-08 23:29:44 +0100

    Refactor: moved constants defined in interfaces to a class

--- src/main/java/org/sitmun/plugin/core/constraints/CodeLists.java
+++ src/main/java/org/sitmun/plugin/core/constraints/CodeLists.java
@@ -1,52 +1,52 @@
 package org.sitmun.plugin.core.constraints;
 
-public interface CodeLists {
+public class CodeLists {
 
-  String APPLICATION_TYPE = "application.type";
+  public static final String APPLICATION_TYPE = "application.type";
 
-  String TERRITORY_SCOPE = "territory.scope";
+  public static final String TERRITORY_SCOPE = "territory.scope";
 
-  String USER_IDENTIFICATION_TYPE = "user.identificationType";
+  public static final String USER_IDENTIFICATION_TYPE = "user.identificationType";
 
-  String CARTOGRAPHY_LEGEND_TYPE = "cartography.legendType";
+  public static final String CARTOGRAPHY_LEGEND_TYPE = "cartography.legendType";
 
-  String CARTOGRAPHY_GEOMETRY_TYPE = "cartography.geometryType";
+  public static final String CARTOGRAPHY_GEOMETRY_TYPE = "cartography.geometryType";
 
-  String CARTOGRAPHY_FILTER_TYPE = "cartographyFilter.type";
+  public static final String CARTOGRAPHY_FILTER_TYPE = "cartographyFilter.type";
 
-  String CARTOGRAPHY_FILTER_VALUE_TYPE = "cartographyFilter.valueType";
+  public static final String CARTOGRAPHY_FILTER_VALUE_TYPE = "cartographyFilter.valueType";
 
-  String CARTOGRAPHY_PARAMETER_FORMAT = "cartographyParameter.format";
+  public static final String CARTOGRAPHY_PARAMETER_FORMAT = "cartographyParameter.format";
 
-  String CARTOGRAPHY_PARAMETER_TYPE = "cartographyParameter.type";
+  public static final String CARTOGRAPHY_PARAMETER_TYPE = "cartographyParameter.type";
 
-  String CARTOGRAPHY_PERMISSION_TYPE = "cartographyPermission.type";
+  public static final String CARTOGRAPHY_PERMISSION_TYPE = "cartographyPermission.type";
 
-  String APPLICATION_PARAMETER_TYPE = "applicationParameter.type";
+  public static final String APPLICATION_PARAMETER_TYPE = "applicationParameter.type";
 
-  String SERVICE_TYPE = "service.type";
+  public static final String SERVICE_TYPE = "service.type";
 
-  String SERVICE_NATIVE_PROTOCOL = "service.nativeProtocol";
+  public static final String SERVICE_NATIVE_PROTOCOL = "service.nativeProtocol";
 
-  String SERVICE_PARAMETER_TYPE = "serviceParameter.type";
+  public static final String SERVICE_PARAMETER_TYPE = "serviceParameter.type";
 
-  String TASK_PARAMETER_TYPE = "taskParameter.type";
+  public static final String TASK_PARAMETER_TYPE = "taskParameter.type";
 
-  String TASK_PARAMETER_FORMAT = "taskParameter.format";
+  public static final String TASK_PARAMETER_FORMAT = "taskParameter.format";
 
-  String DOWNLOAD_TASK_FORMAT = "downloadTask.format";
+  public static final String DOWNLOAD_TASK_FORMAT = "downloadTask.format";
 
-  String DOWNLOAD_TASK_SCOPE = "downloadTask.scope";
+  public static final String DOWNLOAD_TASK_SCOPE = "downloadTask.scope";
 
-  String QUERY_TASK_SCOPE = "queryTask.scope";
+  public static final String QUERY_TASK_SCOPE = "queryTask.scope";
 
-  String USER_POSITION_TYPE = "userPosition.type";
+  public static final String USER_POSITION_TYPE = "userPosition.type";
 
-  String THEMATIC_MAP_TYPE = "thematicMap.type";
+  public static final String THEMATIC_MAP_TYPE = "thematicMap.type";
 
-  String THEMATIC_MAP_VALUE_TYPE = "thematicMap.valueType";
+  public static final String THEMATIC_MAP_VALUE_TYPE = "thematicMap.valueType";
 
-  String THEMATIC_MAP_DESTINATION = "thematicMap.destination";
+  public static final String THEMATIC_MAP_DESTINATION = "thematicMap.destination";
 
-  String THEMATIC_MAP_RANGE_STYLE = "thematicMapRange.style";
+  public static final String THEMATIC_MAP_RANGE_STYLE = "thematicMapRange.style";
 }
