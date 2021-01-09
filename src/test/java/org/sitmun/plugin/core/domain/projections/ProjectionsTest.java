3431d96d64f7e55f25921f0a5f4e19779d00009f
fjlopez <fjlopez@unizar.es>
2021-01-09 03:44:55 +0100

    Merge projection tests

--- src/test/java/org/sitmun/plugin/core/domain/projections/ProjectionsTest.java
+++ src/test/java/org/sitmun/plugin/core/domain/projections/ProjectionsTest.java
@@ -25,13 +25,27 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class UserConfigurationProjectionTest {
+public class ProjectionsTest {
+
+  private static final String APPLICATION_PROJECTION_VIEW =
+      "/api/applications/{0}?projection=view";
 
   private static final String USER_CONFIGURATION_PROJECTION_VIEW_PROPERTY_VALUE =
       "/api/user-configurations?projection=view&{0}={1}";
 
+  private static final String USER_POSITION_PROJECTION_VIEW =
+      "/api/user-positions/{0}?projection=view";
+
   @Autowired
   private MockMvc mvc;
+
+  @Test
+  @WithMockUser(username = SITMUN_ADMIN_USERNAME)
+  public void projectionHasTerritoryNames() throws Exception {
+    mvc.perform(get(USER_POSITION_PROJECTION_VIEW, 2124))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.territoryName").value("-  Província de Barcelona -"));
+  }
 
   @Test
   @WithMockUser(username = SITMUN_ADMIN_USERNAME)
@@ -62,6 +76,49 @@ public class UserConfigurationProjectionTest {
         .andExpect(jsonPath("$._embedded.user-configurations", hasSize(34)))
         .andExpect(
             jsonPath("$._embedded.user-configurations[*].id", hasSize(34)));
+  }
+
+
+  @Test
+  @WithMockUser(username = SITMUN_ADMIN_USERNAME)
+  public void checkCartographyGroupNameExistsInProjection() throws Exception {
+    mvc.perform(get("/api/backgrounds?projection=view"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$._embedded.backgrounds[?(@.cartographyGroupName)]", hasSize(6)));
+  }
+
+  @Test
+  @WithMockUser(username = SITMUN_ADMIN_USERNAME)
+  public void checkCartographyGroupIdExistsInProjection() throws Exception {
+    mvc.perform(get("/api/backgrounds?projection=view"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$._embedded.backgrounds[?(@.['cartographyGroup.id'])]", hasSize(6)));
+  }
+
+  @Test
+  @WithMockUser(username = SITMUN_ADMIN_USERNAME)
+  public void projectionHasSituationMapId() throws Exception {
+    mvc.perform(get(APPLICATION_PROJECTION_VIEW, 1))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.situationMapId").value(132));
+  }
+
+  @Test
+  @WithMockUser(username = SITMUN_ADMIN_USERNAME)
+  public void checkTaskGroupNameExistsInTasksProjection() throws Exception {
+    mvc.perform(get("/api/tasks?projection=view"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$._embedded.*.*", hasSize(1756)))
+        .andExpect(jsonPath("$._embedded.*.[?(@.groupName)]", hasSize(1756)));
+  }
+
+  @Test
+  @WithMockUser(username = SITMUN_ADMIN_USERNAME)
+  public void checkCartographyGroupIdExistsInTasksProjection() throws Exception {
+    mvc.perform(get("/api/tasks?projection=view"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$._embedded.*.*", hasSize(1756)))
+        .andExpect(jsonPath("$._embedded.*.[?(@.['group.id'])]", hasSize(1756)));
   }
 
   @TestConfiguration
