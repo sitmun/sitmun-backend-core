e897796bbf9d2bc2611ba55b7a59a3477c60687d
fjlopez <fjlopez@unizar.es>
2022-03-07 22:35:02 +0100

    Refactor: remove duplicate bean

--- app/src/main/java/org/sitmun/config/CoreConfig.java
+++ app/src/main/java/org/sitmun/config/CoreConfig.java
@@ -3,8 +3,6 @@ package org.sitmun.config;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
@@ -12,11 +10,6 @@ import javax.persistence.EntityManager;
 
 @Configuration
 public class CoreConfig {
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
 
   @Bean
   public Validator validator() {
