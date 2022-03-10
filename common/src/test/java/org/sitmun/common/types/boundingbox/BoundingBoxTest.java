ca1c65125a9478e1e1a166fba0861ae5ea2eb6e6
fjlopez <fjlopez@unizar.es>
2022-03-10 15:29:23 +0100

    #132 Move SRS tests

--- common/src/test/java/org/sitmun/common/types/boundingbox/BoundingBoxTest.java
+++ common/src/test/java/org/sitmun/common/types/boundingbox/BoundingBoxTest.java
@@ -1,47 +1,23 @@
 package org.sitmun.common.types.boundingbox;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.sitmun.common.domain.territory.Territory;
 import org.sitmun.common.types.envelope.Envelope;
+import org.sitmun.test.BaseTest;
 import org.sitmun.test.URIConstants;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DisplayName("BoundingBox validation test")
-public class BoundingBoxTest {
-
-  @Autowired
-  private WebApplicationContext context;
-
-  private MockMvc mvc;
-
-  @BeforeEach
-  public void setup() {
-    mvc = MockMvcBuilders
-      .webAppContextSetup(context)
-      .apply(springSecurity())
-      .build();
-  }
+public class BoundingBoxTest extends BaseTest {
 
   @Test
   @DisplayName("Pass if meets conditions of BoundingBox")
---
+++ common/src/test/java/org/sitmun/common/types/srs/SpatialReferenceSystemTest.java
@@ -0,0 +1,94 @@
+package org.sitmun.common.types.srs;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.sitmun.common.types.codelist.CodeListValidator;
+import org.sitmun.common.types.codelist.CodeListValueRepository;
+import org.sitmun.test.BaseTest;
+import org.sitmun.test.URIConstants;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("SpatialReferenceSystem validation test")
+@Import(CodeListValidator.class)
+public class SpatialReferenceSystemTest extends BaseTest {
+
+  @MockBean
+  private CodeListValueRepository repository;
+
+  @BeforeEach
+  public void mocks() {
+    when(repository.existsByCodeListNameAndValue(any(String.class), any(String.class))).thenReturn(true);
+  }
+
+  @Test
+  @DisplayName("Single projection pass")
+  @WithMockUser(roles = {"ADMIN"})
+  public void singleProjectionPass() throws Exception {
+    mvc.perform(post(URIConstants.SERVICE_URI)
+        .contentType(APPLICATION_JSON)
+        .content(serviceFixture("EPSG:1"))
+      )
+      .andExpect(status().isCreated())
+      .andReturn().getResponse().getHeader("Location");
+  }
+
+  @Test
+  @DisplayName("Single other value fail")
+  @WithMockUser(roles = {"ADMIN"})
+  public void singleOtherValueFail() throws Exception {
+    mvc.perform(post(URIConstants.SERVICE_URI)
+        .contentType(APPLICATION_JSON)
+        .content(serviceFixture("other")))
+      .andExpect(status().isBadRequest())
+      .andExpect(jsonPath("$.errors[0].property").value("supportedSRS"))
+      .andExpect(jsonPath("$.errors[0].invalidValue[0]").value("other"));
+  }
+
+  @Test
+  @DisplayName("Multiple projections pass")
+  @WithMockUser(roles = {"ADMIN"})
+  public void multipleProjectionPass() throws Exception {
+    mvc.perform(post(URIConstants.SERVICE_URI)
+        .contentType(APPLICATION_JSON)
+        .content(serviceFixture("EPSG:1", "EPSG:2"))
+      )
+      .andExpect(status().isCreated());
+  }
+
+  @Test
+  @DisplayName("Multiple projections with other value fail")
+  @WithMockUser(roles = {"ADMIN"})
+  public void multipleProjectionsWithOtherValueFail() throws Exception {
+    mvc.perform(post(URIConstants.SERVICE_URI)
+        .contentType(APPLICATION_JSON)
+        .content(serviceFixture("EPSG:1", "other")))
+      .andExpect(status().isBadRequest())
+      .andExpect(jsonPath("$.errors[0].property").value("supportedSRS"))
+      .andExpect(jsonPath("$.errors[0].invalidValue[0]").value("EPSG:1"))
+      .andExpect(jsonPath("$.errors[0].invalidValue[1]").value("other"));
+  }
+
+  public String serviceFixture(String... projection) throws JSONException {
+    return new JSONObject()
+      .put("supportedSRS", new JSONArray(projection))
+      .put("type", "WMS")
+      .put("blocked", "false")
+      .put("name", "any name")
+      .put("serviceURL", "http://example.com/")
+      .toString();
+  }
+
+}
