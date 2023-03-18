942c80efe18235482f0b9905020dbf7ca870f973
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2023-03-18 23:25:11 +0100

    #149 Make primary springDocConfiguration in ProvidedOpenApiConfigurer

--- common/src/main/java/org/sitmun/common/web/ProvidedOpenApiConfigurer.java
+++ common/src/main/java/org/sitmun/common/web/ProvidedOpenApiConfigurer.java
@@ -5,12 +5,18 @@ import org.springdoc.core.SpringDocConfiguration;
 import org.springdoc.core.providers.ObjectMapperProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile({"openapi-provided"})
 public class ProvidedOpenApiConfigurer {
 
+  @Bean
+  @Primary
+  SpringDocConfiguration springDocConfiguration(){
+    return new SpringDocConfiguration();
+  }
   @Bean
   SpringDocConfigProperties springDocConfigProperties() {
     return new SpringDocConfigProperties();
