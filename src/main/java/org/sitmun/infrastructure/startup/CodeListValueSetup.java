9613acc1797e5df42918fd14bc2991b4a8141e70
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2025-02-21 11:36:00 +0100

    feat #200: ensured system code list values are always present

---
+++ src/main/java/org/sitmun/infrastructure/startup/CodeListValueSetup.java
@@ -0,0 +1,58 @@
+package org.sitmun.infrastructure.startup;
+
+import lombok.extern.slf4j.Slf4j;
+import org.sitmun.infrastructure.persistence.type.codelist.CodeListValue;
+import org.sitmun.infrastructure.persistence.type.codelist.CodeListValueRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Component
+@ConditionalOnProperty(prefix = "sitmun.startup.code-lists", name = "check-on-startup", havingValue = "true", matchIfMissing = true)
+public class CodeListValueSetup implements ApplicationRunner {
+
+  private StartupCodelistsProperties properties;
+  private CodeListValueRepository repository;
+
+  public CodeListValueSetup(StartupCodelistsProperties properties, CodeListValueRepository repository) {
+    this.properties = properties;
+    this.repository = repository;
+  }
+
+  @Override
+  @Transactional
+  public void run(ApplicationArguments args) {
+    log.info("Started CodeListValueSetup");
+    Map<String, Map<String, CodeListValue>> existingConfiguration = new HashMap<>();
+    repository.findAll().forEach(item -> {
+      if (!existingConfiguration.containsKey(item.getCodeListName())) {
+        existingConfiguration.put(item.getCodeListName(), new HashMap<>());
+      }
+      existingConfiguration.get(item.getCodeListName()).put(item.getValue(), item);
+    });
+    List<CodeListValue> newValues = new ArrayList<>();
+    properties.getCodeListValues().forEach( item -> {
+      Map<String, CodeListValue> currentList = existingConfiguration.get(item.getCodeListName());
+      if (currentList == null || !currentList.containsKey(item.getValue())) {
+        CodeListValue codeListValue = new CodeListValue();
+        codeListValue.setCodeListName(item.getCodeListName());
+        codeListValue.setValue(item.getValue());
+        codeListValue.setDescription(item.getDescription());
+        codeListValue.setDefaultCode(item.isDefault());
+        codeListValue.setSystem(true);
+        newValues.add(codeListValue);
+        log.info("Added code list value {}:{}", item.getCodeListName(), item.getValue());
+      }
+    });
+    repository.saveAll(newValues);
+    log.info("Finished CodeListValueSetup: added {}", newValues.size());
+  }
+}
---
+++ src/main/java/org/sitmun/infrastructure/startup/StartupCodelistsProperties.java
@@ -0,0 +1,37 @@
+package org.sitmun.infrastructure.startup;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "sitmun.startup.code-lists")
+@Setter
+@Getter
+public class StartupCodelistsProperties {
+
+  private boolean checkOnStartup = true;
+  private List<DatabaseConnectionDriverDefinition> databaseConnectionDrivers = Collections.emptyList();
+  private List<CodeListValueDefinition> codeListValues = Collections.emptyList();
+
+  @Getter
+  @Setter
+  public static class DatabaseConnectionDriverDefinition {
+    private String driverClass;
+    private String description;
+  }
+
+  @Getter
+  @Setter
+  public static class CodeListValueDefinition {
+    private String codeListName;
+    private String value;
+    private String description;
+    private boolean isDefault = false;
+  }
+
+}
