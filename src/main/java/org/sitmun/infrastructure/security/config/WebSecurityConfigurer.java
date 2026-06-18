d05820629fd2170216522f29b046218da803488b
olamelas <olamelas@nexusgeographics.com>
2026-06-18 12:07:52 +0200

    feat(backend): Add render endpoint to USER available routes

--- src/main/java/org/sitmun/infrastructure/security/config/WebSecurityConfigurer.java
+++ src/main/java/org/sitmun/infrastructure/security/config/WebSecurityConfigurer.java
@@ -277,6 +277,9 @@ public class WebSecurityConfigurer {
         .hasRole(USER.name())
         .requestMatchers(builder.matcher(HttpMethod.GET, "/api/user/details"))
         .hasRole(USER.name())
+        .requestMatchers(
+            builder.matcher(HttpMethod.POST, "/api/tasks/template/more-info-advanced/render"))
+        .hasRole(USER.name())
         .requestMatchers(builder.matcher(HttpMethod.POST, "/api/authenticate/proxy"))
         .hasRole(USER.name());
   }
